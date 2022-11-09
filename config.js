1580514614176de6d6a31a9730702d5bb4499c14
pkfranz <paulkfranz@gmail.com>
2022-11-09 11:39:33 -0500

    initial map init

---
+++ config.js
@@ -0,0 +1,86 @@
+var config = {
+    style: 'mapbox://styles/daltonwb/cla9v6wtm000l14nvdixna87z',
+    accessToken: 'pk.eyJ1IjoiZGFsdG9ud2IiLCJhIjoiOWdSSXFQSSJ9.HZyjh4g3TAAOAncwelv9Vw',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    inset: false,
+    theme: 'light',
+    projection: 'globe',
+    use3dTerrain: false,
+    title: '<a href="https://www.crisisgroup.org" target="_blank"><img id="icg" src="images/icg-oj.png" /></a><br /><video id="background-video" autoplay loop muted poster="images/poster.jpg"><source src="images/aerial.mp4" type="video/mp4"></video><h1>Taiz in a Knot</h1><p class="hedp">Thanks to a UN-brokered truce, Yemen has enjoyed a protracted period of relative calm over the past six months, the first such lull since the civil war broke out almost eight years ago.</p><p class="hedp"><strong>The UN has proposed a six-month, expanded truce. But wrangling over a deal to reopen Huthi-controlled roads in and around the city of Taiz – and Huthi demands for salary payments as an inducement to continue the détente - has pushed the truce to the verge of collapse. If an agreement is not struck the conflict may return to, or even exceed, previous levels of destructive intensity, and the prospects of a negotiated settlement will become more distant.</p><a id="freeExplore" href="#INTRO"> BEGIN &nbsp;&nbsp; &#8595; </a><p id="vidcredit">Video: © <a href="https://www.unhcr.org/en-us/news/stories/2022/3/6242bef84/south-sudanese-battle-record-floods-amid-rapidly-changing-climate.html" target="_blank">UNHCR</a></p>',
+    subtitle: '',
+    byline: '',
+    footer: '<h4>Credits</h4><strong>Lead Contributors</strong>: Peter Salisbury<br /><strong>Visualizations & Design</strong>: Paul Franz and Claire Boccon-Gibod<br /><strong>Support</strong>: Kevin Mazur and Veena Ali-Khan<br /><br /><h4>Data Sources</h4> <div class="data-sources"><br /><br /><a href="https://www.crisisgroup.org/legal" target="_blank">Privacy Policy & Legal</a></div>',
+    chapters: [
+      {
+          id: 'INTRO',
+          alignment: 'left',
+          hidden: false,
+          title: 'Yemen’s Major Cities',
+          image: '',
+          description: 'Before the ongoing civil war broke out in early 2015, Taiz City was Yemen’s second most populous urban centre.' + '<br /><br />' + '<strong>Rotate the globe by clicking and dragging your pointer, or use the controls in the top right. Throughout this story, you can freely explore the maps where there is a right arrow. &#8594;' + '<br /><br />' + 'Or scroll down to continue. &#8595;</strong>',
+          location: {
+            center: [25.24688, 3.93630],
+            zoom: 2,
+            pitch: 0,
+            bearing: 0
+          },
+          mapAnimation: 'flyTo',
+          rotateAnimation: true,
+          callback: '',
+          onChapterEnter: [
+
+          ],
+          onChapterExit: [
+
+        ]
+      },
+      {
+          id: '01',
+          alignment: 'left',
+          hidden: false,
+          title: ' ',
+          image: '',
+          description: '<strong>In South Sudan, unprecedented floods have submerged large swathes of the country</strong> and displaced hundreds of thousands of people. In 2021 alone, over half a million people were <span class="green">displaced internally</span>. The 2022 floods, which have just begun, likely will affect even more. Making matters worse, the flooding has helped drive conflict far to the south, in the country’s Equatoria region. There, newly and previously displaced herders, as well as those who migrated with them, are fighting with long-time resident farmers over land. Dozens have died in fighting over the past year and, unless conflict drivers are addressed, tensions could well increase.' + '<h3 class="white"><span style="background-color: #7e6059; font-size: 24px; font-weight: 600; border-radius: 8px; padding: 5px 10px 5px 10px;">Flood Affected People by State</span></h3><div class="video-container"><iframe src="https://flo.uri.sh/visualisation/11468795/embed" title="Flood Affected People by State" class="flourish-embed-iframe" frameborder="0" scrolling="no" sandbox="allow-same-origin allow-forms allow-scripts allow-downloads allow-popups allow-popups-to-escape-sandbox allow-top-navigation-by-user-activation"></iframe></div>',
+          location: {
+            center: [28.21862, 7.51420],
+            zoom: 5.79,
+            pitch: 20.50,
+            bearing: 13.60,
+            speed: .4
+          },
+          mapAnimation: 'flyTo',
+          rotateAnimation: false,
+          callback: '',
+          onChapterEnter: [
+
+          ],
+          onChapterExit: [
+
+        ]
+      },
+        {
+            id: 'NARRATIVE-END',
+            alignment: 'full',
+            hidden: false,
+            title: 'Recommendations',
+            image: '',
+            description: '<strong>External partners and the UN should work with South Sudanese authorities, religious leaders, community elders and others to de-escalate tensions and create security for both displaced and host communities, while protecting the needs of both</strong>.' + '<div class="spacer"></div>' + '<strong>Authorities should try to ease political tensions between the displaced and host communities</strong>, such as by organising a conference of Jonglei and Equatorian leaders to agree on basic modalities in the likely event of future displacement, including potential migration corridors, grazing land, timetables for returns, protections for farmers and host communities, and adjudication mechanisms. Though this dialogue will be contentious given the heated politics, the alternative is worsening violence as climate-driven risk factors continue to increase tensions. External partners can support such processes or fill in gaps should the authorities not take up the task. ' + '<div class="spacer"></div>' + '<img src="images/sam_poster.jpg" />' + '<div class="spacer"></div>' + 'Aid groups should pre-position assistance for the displaced and help them return home. In addition to honouring their commitments to provide climate adaptation funding, donor countries should offer technical experts to work with national and local South Sudanese authorities. These experts could work on helping communities to mitigate the flooding’s harm while protecting the ecosystems preserving traditional livelihoods that rely on seasonal floods. Donor countries should then offer to fund these flood-mitigation strategies, while taking care that the funds do not simply disappear amid the <a href="https://www.crisisgroup.org/africa/horn-africa/south-sudan/305-oil-or-nothing-dealing-south-sudans-bleeding-finances" target="_blank">extreme corruption</a> that characterises South Sudan’s young state.' + '<div class="spacer"></div>' + 'Finally, even while addressing the climate-driven risks described above, <strong>outside stakeholders should not lose sight of the broader political context</strong>, which remains fraught. <a href="https://www.crisisgroup.org/africa/horn-africa/south-sudan/b169-south-sudans-other-war-resolving-insurgency-equatoria" target="_blank">They should refocus</a> their attention on resolving the conflict in the Equatoria region, including by resuscitating the stalled peace talks in Rome between the South Sudanese government and the Equatoria-based National Salvation Front insurgency.' + '<div class="spacer"></div>' + '<strong>Read more of Crisis Group’s work on climate, environment and conflict <a href="https://www.crisisgroup.org/future-conflict/climate-environment-and-conflict" target="_blank">here</a></strong>.',
+            location: {
+              center: [31.73713, 7.21163],
+              zoom: 5,
+              pitch: 44.00,
+              bearing: 23.20,
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+
+            ],
+            onChapterExit: [
+
+          ]
+        }
+    ]
+};
